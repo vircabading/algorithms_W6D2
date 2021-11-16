5af92adaf00966e849aeb7b313aadd174ed46ed4
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-16 08:55:43 -0800

    initialize

---
+++ module.js
@@ -0,0 +1,7 @@
+export function print_desc(description) {                     // Function to print descriptions onto the console
+    console.log (":::: " + description + " :::::::::::::::::::::::::::::::::::::::::")
+}
+
+export function print_ln() {                                   // Function to print an empty line 
+    console.log("");
+}
